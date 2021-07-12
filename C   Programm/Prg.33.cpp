e6061b91cbd81f8c36e245c272b5471b0b64df6a
Sumit2790 <86053674+Sumit2790@users.noreply.github.com>
2021-07-12 18:26:28 +0530

    Divide two numbers and print:

---
+++ C   Programm/Prg.33.cpp
@@ -0,0 +1,13 @@
+//Divide two numbers and print:
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a=30;
+	int b=10;
+	int z;
+	z=a/b;
+	cout << " The quotient of "<< a << " and "<<b <<" is : "<< z <<"\n\n" ;
+}
